2658de5056b6441140af2f8518577089af1c7b94
IkeuchiYura <k020g1233@g.neec.ac.jp>
2023-09-02 21:56:37 +0900

    数学クラスを作り直した

--- SourceCode/engine/math/Vector3.h
+++ SourceCode/engine/math/Vector3.h
@@ -1,41 +1,41 @@
-//#pragma once
-//#include <DirectXMath.h>
-//
-//class Vector3 {
-//public:
-//	float x;
-//	float y;
-//	float z;
-//
-//	Vector3();
-//	Vector3(float x, float y, float z);
-//
-//	float length() const;
-//	Vector3& normalize();
-//	float dot(const Vector3& v) const;
-//	Vector3 cross(const Vector3& v) const;
-//
-//	Vector3 operator+() const;
-//	Vector3 operator-() const;
-//
-//	Vector3& operator+=(const Vector3& v);
-//	Vector3& operator-=(const Vector3& v);
-//	Vector3& operator*=(float s);
-//	Vector3& operator/=(float s);
-//
-//	// 補間関数
-//	// 線形補間(1次関数補間)
-//	static const Vector3 lerp(const Vector3& start, const Vector3& end, const float t);
-//	// ease-in補間
-//	static const Vector3 easeIn(const Vector3& start, const Vector3& end, const float t);
-//	// ease-out補間
-//	static const Vector3 easeOut(const Vector3& start, const Vector3& end, const float t);
-//	// ease-in, out補間
-//	static const Vector3 easeInOut(const Vector3& start, const Vector3& end, const float t);
-//};
-//
-//const Vector3 operator+(const Vector3& v1, const Vector3& v2);
-//const Vector3 operator-(const Vector3& v1, const Vector3& v2);
-//const Vector3 operator*(const Vector3& v, float s);
-//const Vector3 operator*(float s, const Vector3& v);
-//const Vector3 operator/(const Vector3& v, float s);
+#pragma once
+#include <DirectXMath.h>
+
+class Vector3 : public DirectX::XMFLOAT3
+{
+public:
+	Vector3();
+	Vector3(float x, float y, float z);
+
+	float length() const;
+	Vector3& normalize();
+	float dot(const Vector3& v) const;
+	Vector3 cross(const Vector3& v) const;
+	float angle(const Vector3& v) const;
+
+	//XMVECTOR系の変換関数
+	Vector3 DirectXVector3Transform(const DirectX::XMMATRIX& v) const;
+
+	//単項演算子オーバーロード
+	Vector3 operator+() const;
+	Vector3 operator-() const;
+
+	//代入演算子オーバーロード
+	Vector3& operator+=(const Vector3& v);
+	Vector3& operator-=(const Vector3& v);
+	Vector3& operator*=(float s);
+	Vector3& operator/=(float s);
+};
+
+//二項演算子オーバーロード
+const Vector3 operator+(const Vector3& v1, const Vector3& v2);
+const Vector3 operator-(const Vector3& v1, const Vector3& v2);
+const Vector3 operator*(const Vector3& v, float s);
+const Vector3 operator*(float s, const Vector3& v);
+const Vector3 operator/(const Vector3& v, float s);
+
+//線形補間
+const Vector3 lerp(const Vector3& start, const Vector3& end, const float time);
+const Vector3 easeIn(const Vector3& start, const Vector3& end, const float time);
+const Vector3 easeOut(const Vector3& start, const Vector3& end, const float time);
+const Vector3 easeInOut(const Vector3& start, const Vector3& end, const float time);
